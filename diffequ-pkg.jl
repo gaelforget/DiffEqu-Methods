dae34da4996ad3b6d1d950df593cca2221737b19
Luis Becerra Solis <48189742+lubeso@users.noreply.github.com>
2019-03-13 11:29:10 -0400

    rm depricated de-p.jl

--- diffequ-pkg.jl
+++
@@ -1,9 +0,0 @@
-using DifferentialEquations;
-using Plots; gr();
-C = randn(1)[1];
-f(u,p,t) = cos(2*pi*u);
-u0 = 0.0;
-tspan = (0.0,10.0);
-prob = ODEProblem(f,u0,tspan)
-sol = solve(prob,saveat=0.1);
-plot(sol,label="Numerical Solution")
